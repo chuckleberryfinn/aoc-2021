5dc872b5b26da8c41ba16dc3da75c63bab8cb13d
chuckleberryfinn <chuckeberryfinn@gmail.com>
2021-12-01 15:25:02 +0000

    Cleanup a little

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -10,23 +10,26 @@ fn get_inputs() -> Vec<i32> {
 }
 
 
-fn get_result() -> usize {
-    get_inputs().windows(2)
+fn get_sums(input: &[i32]) -> Vec<i32> {
+    input
+        .windows(3)
+        .into_iter()
+        .map(|x| x.iter().sum())
+        .collect::<Vec<i32>>()
+}
+
+
+fn get_result(input: &[i32]) -> usize {
+    input
+        .windows(2)
         .filter(|w| w[1] > w[0])
         .count()
 }
 
 
-fn get_result_2() -> usize {
-    get_inputs().windows(4)
-        .filter(|w| w[1..4].iter().sum::<i32>() > w[0..3].iter().sum::<i32>())
-        .count()
-}
-
-
 fn main() -> Result<()> {
-    println!("Part 1 {}", get_result());
-    println!("Part 2 {}", get_result_2());
+    println!("Part 1 {}", get_result(&get_inputs()));
+    println!("Part 2 {}", get_result(&get_sums(&get_inputs())));
     Ok(())
 }
 
@@ -37,12 +40,12 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert!(get_result() == 1_451);
+        assert!(get_result(&get_inputs()) == 1_451);
     }
 
     
     #[test]
     fn test_part_2() {
-        assert!(get_result_2() == 1_395);
+        assert!(get_result(&get_sums(&get_inputs())) == 1_395);
     }
 }
