30aa189b52aee36752e4321f8831ea96dffe483c
chuckleberryfinn <chuckeberryfinn@gmail.com>
2021-12-11 12:44:49 +0000

    Cleanup lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,2 @@
-use std::fs::File;
-use std::io;
-use std::io::prelude::*;
-use std::path::Path;
-
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-pub fn input(name: &str) -> io::Result<String> {
-    let mut s = String::new();
-    File::open(Path::new("input").join(name))?.read_to_string(&mut s)?;
-    Ok(s)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::input;
-
-    #[test]
-    fn test_input() {
-        assert!(input("tests.txt").unwrap() == "test");
-    }
-}
