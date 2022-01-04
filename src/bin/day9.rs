b3109fe11650672e5223a7718a9709888a54808f
chuckleberryfinn <chuckeberryfinn@gmail.com>
2022-01-04 18:43:12 +0000

    Initial Day 9

---
+++ src/bin/day9.rs
@@ -0,0 +1,106 @@
+use aoc::*;
+use std::collections::{BinaryHeap, HashSet};
+
+
+fn get_inputs() -> Vec<Vec<u32>> {
+    include_str!("../../input/day9.txt")
+        .lines()
+        .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect())
+        .collect::<Vec<Vec<u32>>>()
+}
+
+
+fn all_lows(inputs: &[Vec<u32>]) -> Vec<(usize, usize)> {
+    inputs
+        .iter()
+        .enumerate()
+        .map(|(i, row)|
+            row
+                .iter()
+                .enumerate()
+                .filter(|(j, column)| !((i > 0 && **column >= inputs[i-1][*j])
+                                        || (*j > 0 && **column >= inputs[i][j-1])
+                                        || (i + 1 < inputs.len() && **column >= inputs[i+1][*j])
+                                        || (j + 1 < inputs[0].len() && **column >= inputs[i][j+1]))
+                )
+                .into_iter()
+                .map(|(j, _)| (i, j))
+                .collect::<Vec<(usize, usize)>>()
+        )
+        .flatten()
+        .collect()
+}
+
+
+fn part_1() -> u32 {
+    let inputs = get_inputs();
+
+    all_lows(&inputs)
+        .iter()
+        .map(|(x, y)| inputs[*x][*y] + 1)
+        .collect::<Vec<u32>>()
+        .iter()
+        .sum()
+}
+
+
+fn basin(inputs: &[Vec<u32>], start: (usize, usize), seen: &mut HashSet<(usize, usize)>) -> usize {
+    if start.0 >= inputs.len() || start.1 >= inputs[0].len() || seen.contains(&(start.0, start.1)) {
+        return 0;
+    }
+
+    seen.insert((start.0, start.1));
+
+    if inputs[start.0][start.1] == 9 {
+        return 0;
+    }
+
+    let mut x = 1 + basin(inputs, (start.0 + 1, start.1), seen) + basin(inputs, (start.0, start.1 + 1), seen);
+    if start.0 > 0 {
+        x += basin(inputs, (start.0 - 1, start.1), seen);
+    }
+
+    if start.1 > 0 {
+        x += basin(inputs, (start.0, start.1 - 1), seen);
+    }
+
+    x
+}
+
+
+fn part_2() -> usize {
+    let inputs = get_inputs();
+    let mut seen = HashSet::new();
+
+    let all_basins =
+        all_lows(&inputs)
+            .iter()
+            .map(|l| basin(&inputs, *l, &mut seen))
+            .collect::<BinaryHeap<usize>>();
+
+    all_basins.iter().take(3).product::<usize>()
+}
+
+
+fn main() -> Result<()> {
+    println!("{}", part_1());
+    println!("{}", part_2());
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert!(part_1() == 532);
+    }
+
+    
+    #[test]
+    fn test_part_2() {
+        assert!(part_2() == 1_110_780);
+    }
+}
